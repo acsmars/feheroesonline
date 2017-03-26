0c691496ad2d04920b71fac543cb0687031e70ae
Jordan Wandling <Fi@Jordans-MacBook-Pro.local>
2017-03-26 15:12:08 +0900

    initiate unit skeleton

--- client/js/game.js
+++ client/js/game.js
@@ -10,11 +10,10 @@ function gameTileClick(x,y) {
 
 }
 
-function gameMapTile(type,wallHealth) {
+function gameMapTile(type,wallHealth,unit) {
 	// Prototype to be instanced for each tile in the map array
 	this.type;  // plains, trees, mountains, water 0,1,2,3
 	this.wallHealth = wallHealth; // -1 -> Indestructable Wall | 0 -> No Wall | >1 -> Wall with that much health
-	this.unit = null;
 	this.effect; // To be added later...
 
 	// Load terrain type
@@ -32,14 +31,37 @@ function gameMapTile(type,wallHealth) {
 			this.type = 'water';
 			break;
 	}
+	
+	this.unit = unit;
+	
+}
+
+function gameInitiateUnit(unitIndex) {
+	if ((unitIndex > -1) && (unitIndex < unitList.length)) {
+		this.stats = unitList[unitIndex].stats;
+		this.character = unitList[unitIndex].character ;
+		this.movementType = unitList[unitIndex].movementType; //Cavalry/Infantry/etc
+		this.weapon = unitList[unitIndex].weapon;
+		this.alive = true;
+		this.active = false;
+		/*
+			Should load all data for each unit directly from teambuilder
+			Prior to this, that information would be stored in an array of objects
+			The object's starting position would be determined by its index in that list
+			That index (0, 1, 8 or w/e) would be stored in the map we load in the third index of the array
+		*/
+		return this;
+	}
+	return null;
 }
 
 function gameLoadMap(mapArray) {
 	map = [];
 	for (var y = 0; y < mapArray.length; y++) {
-		map[map.length] = []
+		map[y] = [];
 		for (var x = 0; x < mapArray[0].length; x++) {
-			map[y][x] = new gameMapTile(mapArray[y][x][0],mapArray[y][x][1]);
+			var loadUnit = new gameInitiateUnit(mapArray[y][x][2]); //Returns an object if a unit is supposed to start there
+			map[y][x] = new gameMapTile(mapArray[y][x][0],mapArray[y][x][1], loadUnit);
 		}
 	}
 	return map;
